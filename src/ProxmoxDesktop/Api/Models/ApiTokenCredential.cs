8fec7c6af66a192c8312b87c60e26d707ee84b98
Kofy <baki.sagir@ik.me>
2026-06-08 01:57:09 +0200

    cleanup: remove src/

--- src/ProxmoxDesktop/Api/Models/ApiTokenCredential.cs
+++
@@ -1,4 +0,0 @@
-namespace ProxmoxDesktop.Api.Models;
-
-/// <summary>Holds an API token credential (tokenId + secret).</summary>
-internal sealed record ApiTokenCredential(string TokenId, string Secret);
