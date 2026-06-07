85ff904a59650921a618497b0775c8baa95ced2d
Kofy <baki.sagir@ik.me>
2026-06-08 01:58:24 +0200

    cleanup: remove src/ and old WinForms files

--- src/ProxmoxDesktop/Api/Models/DataTicket.cs
+++
@@ -1,8 +0,0 @@
-namespace ProxmoxDesktop.Api.Models;
-
-internal sealed class DataTicket
-{
-    public required string Ticket              { get; init; }
-    public required string CsrfPreventionToken { get; init; }
-    public required string Username            { get; init; }
-}
--- src/ProxmoxDesktop/Api/Models/NodeData.cs
+++
@@ -1,9 +0,0 @@
-using System.Text.Json.Serialization;
-
-namespace ProxmoxDesktop.Api.Models;
-
-public sealed class NodeData
-{
-    [JsonPropertyName("node")]   public string Node   { get; init; } = string.Empty;
-    [JsonPropertyName("status")] public string Status { get; init; } = string.Empty;
-}
