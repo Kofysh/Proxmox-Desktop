3afac400f78dd24f3aa7d00db4c1fd32c960cb3d
Kofy <baki.sagir@ik.me>
2026-06-08 02:06:07 +0200

    cleanup: remove old root files

--- Api/Models/NodeData.cs
+++
@@ -1,9 +0,0 @@
-using System.Text.Json.Serialization;
-
-namespace ProxmoxDesktop.Api.Models;
-
-public record NodeData
-{
-    [JsonPropertyName("node")]   public string Node   { get; init; } = string.Empty;
-    [JsonPropertyName("status")] public string Status { get; init; } = string.Empty;
-}
